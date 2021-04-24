ad6267c0e00b86303bde55558436ae84a8d32805
Lari Oncescu <larioncescu@yahoo.com>
2021-04-24 14:25:27 +0300

    Adding basic styling for menu

--- src/component/menu/menu.component.jsx
+++ src/component/menu/menu.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import * as MS from './menu.style'
 
 export const Menu = () => {
 
   return (
-    <div>
-      
-    </div>
+    <MS.Menu>
+      menu
+    </MS.Menu>
   )
 }
--- src/component/menu/menu.style.js
+++ src/component/menu/menu.style.js
@@ -1,2 +1,7 @@
-import 'styled' from 'styled-components'
+import styled from 'styled-components'
 
+export const Menu = styled.div`
+  width: 100vw;
+  height: 1.5em;
+  background-color: blue;
+`
