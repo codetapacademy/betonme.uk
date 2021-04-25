a3edce226b20a28adffa30a0b22ac9b2afc544b1
Lari Oncescu <larioncescu@yahoo.com>
2021-04-25 23:06:29 +0300

    Add sign in button and box shadow to menu

--- src/component/menu/menu.component.jsx
+++ src/component/menu/menu.component.jsx
@@ -4,9 +4,13 @@ import * as MS from './menu.style'
 export const Menu = () => {
 
   return (
-    <MS.Menu>
-      <MS.Hamburger className='bet-on-memenu'></MS.Hamburger>
-      <MS.CompanyName>Bet<MS.On>On</MS.On>Me</MS.CompanyName>
-    </MS.Menu>
+    <>
+      <MS.Menu>
+        <MS.Hamburger className='bet-on-memenu'></MS.Hamburger>
+        <MS.CompanyName>Bet<MS.On>On</MS.On>Me</MS.CompanyName>
+        <MS.SignInButton>Sign In</MS.SignInButton>
+      </MS.Menu>
+      <MS.SpaceAfterMenu></MS.SpaceAfterMenu>
+    </>
   )
 }
--- src/component/menu/menu.style.js
+++ src/component/menu/menu.style.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
 
 export const Menu = styled.div`
+  background-color: #fff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
   display: flex;
   padding: 2px;
   position: fixed;
+  justify-content: space-around;
   top: 0;
   left: 0;
   right: 0;
+  width: 100%;
 `
 export const CompanyName = styled.h1`
   font-weight: bold;
@@ -25,4 +29,24 @@ export const Hamburger = styled.i`
   display: flex;
   align-items: center;
   margin-right: 10px;
+`
+
+export const SignInButton = styled.button`
+  background-color: #ffd613;
+  padding: 10px;
+  color: #fff;
+  font-weight: bold;
+  font-size: 24px;
+  align-self: center;
+  border-radius: 5px;
+  border: none;
+
+  &:hover {
+    background-color: #000;
+    color: #ffd613;
+  }
+`
+
+export const SpaceAfterMenu = styled.div`
+  margin-top: 100px;
 `
