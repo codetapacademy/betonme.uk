bf8aef8ba68661dc2560c3319b3f24f15f5e4451
Lari Oncescu <larioncescu@yahoo.com>
2021-04-26 11:16:13 +0300

    Make hamburger work on click for sidebar

--- src/component/menu/menu.component.jsx
+++ src/component/menu/menu.component.jsx
@@ -3,14 +3,18 @@ import * as MS from './menu.style'
 
 export const Menu = () => {
 
+  const [sideBar, setSideBar] = useState(false);
+
+  const showSideBar = () => setSideBar(!sideBar);
+
   return (
     <>
       <MS.Menu>
-        <MS.Hamburger className='bet-on-memenu'></MS.Hamburger>
+        <MS.Hamburger className='bet-on-memenu' onClick={showSideBar}></MS.Hamburger>
         <MS.CompanyName>Bet<MS.On>On</MS.On>Me</MS.CompanyName>
         <MS.SignInButton>Sign In</MS.SignInButton>
       </MS.Menu>
-      <MS.SideBarNav></MS.SideBarNav>
+      {sideBar && <MS.SideBarNav></MS.SideBarNav>}
       <MS.SpaceAfterMenu></MS.SpaceAfterMenu>
     </>
   )
