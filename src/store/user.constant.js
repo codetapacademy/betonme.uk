782115c35068d6f692f8471da78849fe29893b19
Lari Oncescu <larioncescu@yahoo.com>
2021-05-01 16:15:19 +0300

    Understanding constants in user

--- src/store/user.constant.js
+++ src/store/user.constant.js
@@ -1,3 +1,4 @@
+//Aici sunt prezente constantele care sunt mai apoi folosite in user.action.js
 export const LOGIN = '[user] LOGIN'
 export const LOGOUT = '[user] LOGOUT'
 export const SETUSER = '[user] SETUSER'
