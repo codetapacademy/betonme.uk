1b58ff9707a1d14fc2da803a51f72ca841ae017b
Lari Oncescu <larioncescu@yahoo.com>
2021-06-19 15:51:03 +0300

    Make footer component

---
+++ src/component/footer/footer.style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Footer = styled.div`
+  background-color: ${({ theme }) => theme.color.darker};
+  color: ${({ theme }) => theme.color.lighter};
+`
