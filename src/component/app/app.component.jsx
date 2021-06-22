9149f5723ad9efcaff56afa3b56f53850289ca20
Lari Oncescu <larioncescu@yahoo.com>
2021-06-22 17:19:42 +0300

    Add auction component to app and reach/router

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
+import { Router } from '@reach/router'
 import theme from '../../config/theme'
 //Provider it's actually a react component
 //It's bassicaly the glue for react and redux
@@ -9,6 +10,7 @@ import store from '../../store'
 //The store holds the state
 import { Menu } from '../menu'
 import { Home } from '../home'
+import { Auction } from '../auctions'
 
 const App = () => {
 
@@ -17,6 +19,7 @@ const App = () => {
       <Provider store={store}>
         <Menu />
         <Home />
+        <Auction />
       </Provider>
     </ThemeProvider>
   )
--- src/component/auctions/auction.component.jsx
+++ src/component/auctions/auction.component.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Auction = () => {
   return(
     <div>
-      
+      This is auction component(page)
     </div>
   )
 }
