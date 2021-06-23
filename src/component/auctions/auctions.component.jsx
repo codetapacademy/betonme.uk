1a35885facffa1f2fedc077137a5ec81298a0dbf
Lari Oncescu <larioncescu@yahoo.com>
2021-06-23 18:38:27 +0300

    Add content to auctions component and style it

--- src/component/auctions/auctions.component.jsx
+++ src/component/auctions/auctions.component.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import * as SA from './auctions.style'
+import { ActionCard } from '../action-card'
 
 export const Auctions = () => {
   return(
-    <div>
-      This is auction component(page)
-    </div>
+    <SA.AuctionsSection>
+      <SA.FirstImage />
+      <SA.SectionTitle>Active Auctions</SA.SectionTitle>
+      <SA.StyledDivider />
+      <SA.StyledGrid>
+        <ActionCard />
+        <ActionCard />
+        <ActionCard />
+        <ActionCard />
+      </SA.StyledGrid>
+    </SA.AuctionsSection>
   )
 }
--- src/component/auctions/auctions.style.js
+++ src/component/auctions/auctions.style.js
@@ -1,1 +1,35 @@
 import styled from 'styled-components'
+import AuctionsImage from '../../asset/images/bet.jpg'
+
+export const AuctionsSection = styled.div`
+  background-color: ${({ theme }) => theme.color.lighter};
+`
+
+export const FirstImage = styled.div`
+  height: 40vh;
+  width: 100%;
+  background-image: url(${AuctionsImage});
+  background-position: center;
+  background-size: cover;
+`
+
+export const SectionTitle = styled.h2`
+  color: ${({ theme }) => theme.color.darker};
+  font-size: 36px;
+  text-align: center;
+  padding: 20px;
+`
+
+export const StyledDivider = styled.div`
+  background-color: ${({ theme }) => theme.color.secondary};
+  height: 5px;
+  width: 100px;
+  margin: 0px auto;
+`
+
+export const StyledGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  padding: 30px;
+  grid-gap: 20px;
+`
