030a4c8157f9417c20c8e3b02d0898bcd2b6070e
Lari Oncescu <larioncescu@yahoo.com>
2021-06-29 12:25:06 +0300

    Make Action Card

--- src/component/auction-page/auction-page.component.jsx
+++ src/component/auction-page/auction-page.component.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from "react";
+import * as S from "./auction-page.style";
+import image from "../../asset/images/phone.jpg";
 
 export const AuctionPage = () => {
-
-  return(
-    <div>This si auction page component</div>
-  )
-}
+  return (
+    <S.AuctionPageBackground>
+      <S.AuctionCard>
+        <S.AuctionTitle>This will page auction title</S.AuctionTitle>
+        <S.StyledImage src={image} alt="Auction image" />
+        <S.DescripitonTitle>Description</S.DescripitonTitle>
+        <S.DescriptionContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel urna
+          lacinia lorem pharetra accumsan quis faucibus sapien. Pellentesque
+          mollis molestie ante, quis molestie est. Fusce iaculis nisi odio.
+          Nulla tincidunt sem vitae tortor aliquet ornare. Etiam vel sapien
+          ultricies, blandit sapien eu, facilisis diam. In volutpat lectus sed
+          felis porttitor bibendum. In tempor suscipit quam non porttitor. Sed
+          quis eros bibendum, egestas tortor nec, posuere sapien. Morbi semper
+          tincidunt mauris, sed dapibus libero. Nam sed turpis at ante efficitur
+          facilisis. Quisque vel mauris nibh. Sed malesuada leo ex, a congue sem
+          cursus eu.
+        </S.DescriptionContent>
+      </S.AuctionCard>
+      <S.RightContainer></S.RightContainer>
+    </S.AuctionPageBackground>
+  );
+};
--- src/component/auction-page/auction-page.style.js
+++ src/component/auction-page/auction-page.style.js
@@ -1,1 +1,47 @@
 import styled from 'styled-components'
+
+export const AuctionPageBackground = styled.div`
+  background-color: ${({ theme }) => theme.color.light};
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  padding: 30px 120px;
+`
+
+export const AuctionCard = styled.div`
+  background-color: #fff;
+  grid-column: span 8;
+`
+
+export const RightContainer = styled.div`
+  grid-column: span 4;
+`
+
+export const AuctionTitle = styled.h2`
+  color: ${({ theme }) => theme.color.darker};
+  font-size: 2rem;
+  padding: 30px;
+`
+
+export const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
+`
+
+export const DescripitonTitle = styled.h2`
+  color: ${({ theme }) => theme.color.darker};
+  font-size: 1.5rem;
+  font-weight: 600;
+  padding-left: 30px;
+`
+
+export const DescriptionContent = styled.p`
+  color: ${({ theme }) => theme.color.darker};
+  font-size: 1rem;
+  padding: 30px;
+`
+
+export const RemainingTime = styled.p`
+  padding: 50px;
+  background-color: #fff;
+  color: ${({ theme }) => theme.color.darker}
+`
